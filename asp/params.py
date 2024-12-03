1e6be058fdff7e992d863df21a41a68f194f2783
tsither <sitherted1@gmail.com>
2024-12-03 15:49:43 +0100

    changed params

--- asp/params.py
+++ asp/params.py
@@ -1,3 +1,3 @@
 # primary=['asp/graph_based/actions.lp','asp/graph_based/graph.lp','asp/graph_based/traverse.lp']
-primary=['asp/amtrak/ted.lp', 'asp/amtrak/track_option.lp']
+primary=['asp/amtrak/ted.lp', 'asp/amtrak/track_option_wait_all.lp']
 secondary=[]
