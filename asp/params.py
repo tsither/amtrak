e601f50a9237cad5da3e91bceed2448953e4975f
tsither <sitherted1@gmail.com>
2025-03-09 21:21:18 +0100

    added weighted graph information

--- asp/params.py
+++ asp/params.py
@@ -2,5 +2,8 @@
 # primary=['asp/action_based/actions.lp','asp/action_based/pathfinding.lp','asp/action_based/transitions.lp']
 
 # primary=['asp/test.lp']
-primary = ['amtrak/base.lp', 'amtrak/track_options.lp']
+primary = ['amtrak/base.lp', 'amtrak/track_options.lp']    # Base solution
+# primary = ['amtrak/graph/WGRB_solve.lp']    # WGR-B solution (without 'action' atoms)
+# primary = ['amtrak/graph/WGRI/solve.lp']    # WGR-I solution - doesn't currently run due to necessity of inserted python script
+
 secondary = []
